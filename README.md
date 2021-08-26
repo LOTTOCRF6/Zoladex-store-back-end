04c7e4c414153bb101c53f599c36d09cf14823a6
LOTTOCRF6 <sithandathuzipho@gmail.com>
2021-08-26 16:02:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Zoladex-store-back-end
+# Zoladex-OS-back-end
+# Zoladex-OS-back-end
+# Zoladex-clothing-back-end
--- zoladex_back-endapp.py
+++ zoladex_back-endapp.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_cors import CORS, cross_origin
 from flask_mail import Mail
+from smtplib import SMTPRecipientsRefused, SMTPAuthenticationError
 
 
 class User(object):
@@ -17,25 +18,37 @@ class User(object):
 
 def init_user_register_table():
     conn = sqlite3.connect('Zoladex.db')
-    print("Opened database successfully")
+    # print("Opened database successfully")
 
-    conn.execute("CREATE TABLE IF NOT EXISTS user_register(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    conn.execute("CREATE TABLE IF NOT EXISTS user_registration(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "first_name TEXT NOT NULL,"
                  "last_name TEXT NOT NULL,"
                  "username TEXT NOT NULL,"
                  "password TEXT NOT NULL,"
                  "address TEXT NOT NULL,"
                  "phone TEXT NOT NULL,"
-                 "email TEXT NOT NULL)")
+                 "email TEXT NOT NULL,"
+                 "datetime)")
     print("user table created successfully")
     conn.close()
 
 
+# creating a login table
+def init_user_login_table():
+    with sqlite3.connect('Zoladex.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS user_login (user_login_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "user_email TEXT NOT NULL,"
+                     "password TEXT NOT NULL,"
+                     "login_date TEXT NOT NULL,"
+                     "CONSTRAINT fk_user FOREIGN KEY (user_email) REFERENCES user_registration(user_id))")
+    print("Login table created successfully.")
+
+
 def init_brand_register_table():
     conn = sqlite3.connect('Zoladex.db')
-    print("Opened database successfully")
+    # print("Opened database successfully")
 
-    conn.execute("CREATE TABLE IF NOT EXISTS zoladex_brand_application(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    conn.execute("CREATE TABLE IF NOT EXISTS zoladex_brand_application(brand_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "ceo_name TEXT NOT NULL,"
                  "ceo_surname TEXT NOT NULL,"
                  "brand_name TEXT NOT NULL,"
@@ -44,13 +57,50 @@ def init_brand_register_table():
                  "social_media_link TEXT NOT NULL,"
                  "office_address TEXT NOT NULL,"
                  "brand_phone TEXT NOT NULL,"
-                 "brand_email TEXT NOT NULL)")
+                 "brand_email TEXT NOT NULL,"
+                 "datetime)")
     print("brands table created successfully")
     conn.close()
 
+def init_brand_products_table():
+    conn = sqlite3.connect('Zoladex.db')
+    print("Opened database successfully")
 
+    conn.execute("CREATE TABLE IF NOT EXISTS brand_products(brand_product_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "product_tittle TEXT NOT NULL,"
+                 "brand_name TEXT NOT NULL,"
+                 "image TEXT NOT NULL,"
+                 "user_id INTEGER"
+                 "price TEXT NOT NULL,"
+                 "size TEXT NOT NULL,"
+                 "colour TEXT NOT NULL,"
+                 "description TEXT NOT NULL,"
+                 "CONSTRAINT fk_product FOREIGN KEY (user_id) REFERENCES user_registration(user_id))")
+    print("brand_products table created successfully")
+    conn.close()
+
+def init_order_products_table():
+    conn = sqlite3.connect('Zoladex.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS order_product(order_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "product_tittle TEXT NOT NULL,"
+                 "brand_name TEXT NOT NULL,"
+                 "image TEXT NOT NULL,"
+                 "price TEXT NOT NULL,"
+                 "size TEXT NOT NULL,"
+                 "colour TEXT NOT NULL,"
+                 "description TEXT NOT NULL)")
+    print("order_products table created successfully")
+    conn.close()
+
+
+init_brand_products_table()
+init_order_products_table()
 init_user_register_table()
+init_user_login_table()
 init_brand_register_table()
+
 
 app = Flask(__name__)
 
@@ -63,7 +113,7 @@ def create_blog():
     if request.method == "POST":
         title = request.form['title']
         content = request.form['content']
-        date_created = datetime.datetime.now()
+        date_created = datetime.now()
 
         with sqlite3.connect('Zoladex.db') as conn:
             cursor = conn.cursor()
@@ -77,7 +127,7 @@ def create_blog():
         return response
 
 
-@app.route('/brand-registration/', methods=["POST"])
+@app.route('/brand-registration/', methods=["POST", "GET"])
 @cross_origin()
 def brand_registration():
     response = {}
@@ -113,11 +163,26 @@ def brand_registration():
             # msg.body = "You have successfully registered"
             # mail.send(msg)
         return response
+    if request.method == "GET":
+        response = {}
+        with sqlite3.connect("Zoladex.db") as conn:
+            cursor = conn.cursor()
+            cursor.row_factory = sqlite3.Row
+            cursor.execute("SELECT * FROM zoladex_brand_application")
+            deals = cursor.fetchall()
+            deal_acc = []
+            for i in deals:
+                deal_acc.append({x: i[x] for x in i.keys()})
+
+        response['status_code'] = 200
+        response['data'] = tuple(deal_acc)
+        return response
 
 
 @app.route('/user-registration/', methods=["POST", "GET"])
 @cross_origin()
 def user_registration():
+
     response = {}
     if request.method == "POST":
 
@@ -128,17 +193,19 @@ def user_registration():
         address = request.form['address']
         phone = request.form['phone']
         email = request.form['email']
+        today_date = datetime.datetime.now()
 
         with sqlite3.connect("Zoladex.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO user_register("
+            cursor.execute("INSERT INTO user_registration("
                            "first_name,"
                            "last_name,"
                            "username,"
                            "password,"
                            "address,"
                            "phone,"
-                           "email) VALUES(?, ?, ?, ?, ?, ?, ?)", (first_name, last_name, username, password, address, phone, email))
+                           "email,"
+                           "datetime) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (first_name, last_name, username, password, address, phone, email, today_date))
             conn.commit()
             response["message"] = "success"
             response["status_code"] = 201
@@ -154,6 +221,137 @@ def user_registration():
             cursor = conn.cursor()
             cursor.row_factory = sqlite3.Row
             cursor.execute("SELECT * FROM user_register")
+            deals = cursor.fetchall()
+            deal_acc = []
+            for i in deals:
+                deal_acc.append({x: i[x] for x in i.keys()})
+
+        response['status_code'] = 200
+        response['data'] = tuple(deal_acc)
+        return response
+
+
+@app.route('/user-login/', methods=["POST"])
+@cross_origin()
+def user_login():
+    response = {}
+    if request.method == "POST":
+        try:
+
+            user_email = request.form['user_email']
+            password = request.form['password']
+            date_created = datetime.datetime.now()
+
+            with sqlite3.connect("Zoladex.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO user_login("
+                               "user_email,"
+                               "password,"
+                               "login_date) VALUES(?, ?, ?)", (user_email, password, date_created))
+                conn.commit()
+                response["message"] = "success"
+                response["status_code"] = 201
+
+                return response
+        except SMTPRecipientsRefused:
+            response["message"] = "Invalid email used"
+            response["status_code"] = 401
+            return response
+        except SMTPAuthenticationError:
+            response["message"] = "Incorrect login details"
+            response["status_code"] = 401
+            return response
+
+
+@app.route('/brand-products/', methods=["POST", "GET"])
+@cross_origin()
+def brand_products():
+    response = {}
+    if request.method == "POST":
+
+        product_tittle = request.form['product_tittle']
+        brand_name = request.form['brand_name']
+        image = request.form['image']
+        price = request.form['price']
+        size = request.form['size']
+        colour = request.form['colour']
+        description = request.form['description']
+
+        with sqlite3.connect("Zoladex.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO brand_products("
+                           "product_tittle,"
+                           "brand_name,"
+                           "image,"
+                           "price,"
+                           "size,"
+                           "colour,"
+                           "description) VALUES(?, ?, ?, ?, ?, ?, ?)", (product_tittle, brand_name, image, price, size, colour, description))
+            conn.commit()
+            response["message"] = "success"
+            response["status_code"] = 201
+
+            # msg = Message('WELCOME', sender='sithandathuzipho@gmail.com', recipients=['sithandathuzipho@gmail.com'])
+            # msg.body = "You have successfully registered"
+            # mail.send(msg)
+        return response
+
+    if request.method == "GET":
+        response = {}
+        with sqlite3.connect("Zoladex.db") as conn:
+            cursor = conn.cursor()
+            cursor.row_factory = sqlite3.Row
+            cursor.execute("SELECT * FROM brand_products")
+            deals = cursor.fetchall()
+            deal_acc = []
+            for i in deals:
+                deal_acc.append({x: i[x] for x in i.keys()})
+
+        response['status_code'] = 200
+        response['data'] = tuple(deal_acc)
+        return response
+
+
+@app.route('/order-products/', methods=["POST", "GET"])
+@cross_origin()
+def order_products():
+    # response = {}
+    # if request.method == "POST":
+    #
+    #     product_tittle = request.form['product_tittle']
+    #     brand_name = request.form['brand_name']
+    #     image = request.form['image']
+    #     price = request.form['price']
+    #     size = request.form['size']
+    #     colour = request.form['colour']
+    #     description = request.form['description']
+    #
+    #     with sqlite3.connect("Zoladex.db") as conn:
+    #         cursor = conn.cursor()
+    #         cursor.execute("INSERT INTO order_products("
+    #                        "product_tittle,"
+    #                        "brand_name,"
+    #                        "image,"
+    #                        "price,"
+    #                        "size,"
+    #                        "colour,"
+    #                        "description) VALUES(?, ?, ?, ?, ?, ?, ?)", (product_tittle, brand_name, image,
+    #                        price, size, colour, description))
+    #         conn.commit()
+    #         response["message"] = "success"
+    #         response["status_code"] = 201
+    #
+    #         # msg = Message('WELCOME', sender='sithandathuzipho@gmail.com', recipients=['sithandathuzipho@gmail.com'])
+    #         # msg.body = "You have successfully registered"
+    #         # mail.send(msg)
+    #     return response
+
+    if request.method == "GET":
+        response = {}
+        with sqlite3.connect("Zoladex.db") as conn:
+            cursor = conn.cursor()
+            cursor.row_factory = sqlite3.Row
+            cursor.execute("SELECT * FROM brand_products")
             deals = cursor.fetchall()
             deal_acc = []
             for i in deals:
